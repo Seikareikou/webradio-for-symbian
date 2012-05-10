9530710548e423238ef0bd7fef7cc7538d9a3d25
vano468 <devnull@localhost>
2012-05-10 07:13:05 +0400

    Sorting favorite stations.

--- favorites.h
+++ favorites.h
@@ -6,19 +6,20 @@
 #include <QList>
 #include <QXmlStreamReader>
 #include <QXmlStreamWriter>
+#include <QtAlgorithms>
 #include <QDebug>
 
 class Favorites
 {
     static const int recentCount = 10;
-
     QList<QXmlStreamAttributes> attributesList;
     QString xmlPath;
     void ReadXmlFile();
     bool CheckStation(QString name, QString url, bool delIt);
-
+    void SortingXmlFile();
+    void SortingAttrList();
 public:
-    Favorites(QString xmlPath);
+    Favorites(QString path);
     void AddToFavorites(QString name, QString url);
     void AddToRecent(QString name, QString url);
 };
