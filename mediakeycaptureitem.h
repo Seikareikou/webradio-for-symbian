65c533e620c02bfb6f3475752b81bf07790edc2b
Ivan Kornilov <vano468@gmail.com>
2012-05-26 10:06:57 +0400

    Returned compiling to the simulator

--- mediakeycaptureitem.h
+++ mediakeycaptureitem.h
@@ -9,22 +9,26 @@
 #include <remconinterfaceselector.h>        // RemConInterfaceBase.lib
 #endif // Q_WS_S60
 
+#ifdef Q_WS_S60
 class MediakeyCaptureItemPrivate;
+#endif // Q_WS_S60
 class MediakeyCaptureItem : public QDeclarativeItem
 {
     Q_OBJECT
+#ifdef Q_WS_S60
 public:
     MediakeyCaptureItem(QDeclarativeItem *parent = 0);
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
-
+#endif // Q_WS_S60
 signals:
     void volumeDownPressed();
     void volumeUpPressed();
-
+#ifdef Q_WS_S60
 private:
     MediakeyCaptureItemPrivate *d_ptr;
 private:    // Friend class definitions
     friend class MediakeyCaptureItemPrivate;
+#endif // Q_WS_S60
 };
 
 #endif // MEDIAKEYCAPTURE_H
